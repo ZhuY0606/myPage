6c11378c41d9dc6cdc70b9379d49dcf03f938d50
zhuying <zying_0606@163.com>
2019-08-05 10:17:49 +0800

    test

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+# 确保脚本抛出遇到的错误
+set -e
+# 生成静态文件
+npm run build
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'update'
+
+# 如果你想要部署到 https://USERNAME.github.io
+git push -f git@github.com:ZhuY0606/ZhuY0606.github.io.git master
+
+# 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
+# git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
+
+cd –
